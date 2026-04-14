6a887d574e83f56004f4d408c94c04707e57c41f
ViniciusChavess <vllc@poli.br>
2026-04-14 15:42:17 -0300

    criando adm e user demo

--- frontend/js/data.js
+++ frontend/js/data.js
@@ -169,8 +169,8 @@ const DB = {
   ];
 
   const members = [
-    { name:'Marcos Costa',   email:'marcos@delta.com',   password:'admin123', role:'Líder do setor', sector:'Propulsão', isAdmin:true,  color:COLORS[0] },
-    { name:'Ana Lopes',      email:'ana@delta.com',      password:'senha123', role:'Membro',         sector:'Propulsão', isAdmin:false, color:COLORS[1] },
+    { name:'Marcos Costa',   email:'vini@delta.com',   password:'admin123', role:'Líder do setor', sector:'Propulsão', isAdmin:true,  color:COLORS[0] },
+    { name:'Ana Lopes',      email:'luiz@delta.com',      password:'senha123', role:'Membro',         sector:'Propulsão', isAdmin:false, color:COLORS[1] },
     { name:'Pedro Ribeiro',  email:'pedro@delta.com',    password:'senha123', role:'Membro',         sector:'Propulsão', isAdmin:false, color:COLORS[2] },
     { name:'Isabela Mendes', email:'isabela@delta.com',  password:'senha123', role:'Membro',         sector:'Propulsão', isAdmin:false, color:COLORS[3] },
     { name:'Thiago Souza',   email:'thiago@delta.com',   password:'senha123', role:'Membro',         sector:'Propulsão', isAdmin:false, color:COLORS[4] },
